a92a3e10843bb8b90f20bf861a7df13072ae2877
BearLin <bear.lin.001@gmail.com>
2015-02-22 04:16:03 -0800

    1. Show hidden files in nerdtree. 2. Add cscope script to init/clean cscope files. 3. Add plugin cscope_maps ad autoload_cscope.

---
+++ bundle/autoload_cscope/update.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Reference: 
+# http://derekingrouville.com/2012/cscope-will-change-the-way-you-code/
+# http://vim.sourceforge.net/scripts/script.php?script_id=157
+
+rm autoload_cscope.vim
+wget http://vim.sourceforge.net/scripts/download_script.php?src_id=14884 -O autoload_cscope.vim
---
+++ bundle/cscope_maps/update.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Reference: http://derekingrouville.com/2012/cscope-will-change-the-way-you-code/
+
+rm cscope_maps.vim
+wget http://cscope.sourceforge.net/cscope_maps.vim
