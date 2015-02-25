8808630395d51737df411c1bbc53f652230244b1
BearLin <bear.lin.001@gmail.com>
2015-02-25 15:56:57 +0800

    1. Update README.md. 2. Move all scripts to script/

--- scripts/update_pre_downloaded_plugins.sh
+++ scripts/update_pre_downloaded_plugins.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # cscope_maps
-cd bundle/cscope_maps/ 
+cd ~/.vim/bundle/cscope_maps/ 
 ./update.sh
 cd -
 
 # autoload_cscope
-cd bundle/autoload_cscope/  
+cd ~/.vim/bundle/autoload_cscope/  
 ./update.sh
 cd -
 
