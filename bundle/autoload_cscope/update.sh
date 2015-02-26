26a35350f6b3eaca6b4aa5442913c3da7c8d9b36
bearlin <bear.lin.001@gmail.com>
2015-02-27 01:43:48 +0800

    Update scripts for working on mac os

--- bundle/autoload_cscope/update.sh
+++ bundle/autoload_cscope/update.sh
@@ -5,7 +5,8 @@
 # http://vim.sourceforge.net/scripts/script.php?script_id=157
 
 rm -rf plugin
-wget http://vim.sourceforge.net/scripts/download_script.php?src_id=14884 -O autoload_cscope.vim
+#wget http://vim.sourceforge.net/scripts/download_script.php?src_id=14884 -O autoload_cscope.vim
+curl -o autoload_cscope.vim http://vim.sourceforge.net/scripts/download_script.php?src_id=14884 
 
 mkdir plugin
 mv autoload_cscope.vim plugin
--- bundle/cscope_maps/update.sh
+++ bundle/cscope_maps/update.sh
@@ -3,7 +3,8 @@
 # Reference: http://derekingrouville.com/2012/cscope-will-change-the-way-you-code/
 
 rm -rf plugin
-wget http://cscope.sourceforge.net/cscope_maps.vim 
+#wget http://cscope.sourceforge.net/cscope_maps.vim 
+curl -o cscope_maps.vim http://cscope.sourceforge.net/cscope_maps.vim 
 
 mkdir plugin
 mv cscope_maps.vim plugin
