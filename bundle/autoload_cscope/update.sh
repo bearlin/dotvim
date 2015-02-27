96f12132a472d907b1c303e905e8235721d49a71
BearLin <bear.lin.001@gmail.com>
2015-02-26 23:28:16 -0800

    Update pre-downloaded vim-script update scripts

--- bundle/autoload_cscope/update.sh
+++ bundle/autoload_cscope/update.sh
@@ -4,10 +4,37 @@
 # http://derekingrouville.com/2012/cscope-will-change-the-way-you-code/
 # http://vim.sourceforge.net/scripts/script.php?script_id=157
 
+hasWget=0
+hasCurl=0
+
+which wget &>/dev/null
+if [ $? -eq 0 ]; then
+  hasWget=1
+fi
+
+which curl &>/dev/null
+if [ $? -eq 0 ]; then
+  hasCurl=1
+fi
+echo "hasWget=$hasWget"
+echo "hasCurl=$hasCurl"
+
+if [ $hasWget == 0 ] && [ $hasCurl == 0 ]; then
+  echo "Both wget/crul commands not found, please install one of them first."
+fi
+
 rm -rf plugin
-#wget http://vim.sourceforge.net/scripts/download_script.php?src_id=14884 -O autoload_cscope.vim
-curl -o autoload_cscope.vim http://vim.sourceforge.net/scripts/download_script.php?src_id=14884 
+if [ $hasWget == 1  ]; then
+  wget http://vim.sourceforge.net/scripts/download_script.php?src_id=14884 -O autoload_cscope.vim
+elif [ $hasCurl == 1  ]; then
+  curl -o autoload_cscope.vim http://vim.sourceforge.net/scripts/download_script.php?src_id=14884 
+else
+  echo "Unknow parameters, exit"
+  exit 0
+fi
 
 mkdir plugin
 mv autoload_cscope.vim plugin
+
+echo "bye!"
 
--- bundle/cscope_maps/update.sh
+++ bundle/cscope_maps/update.sh
@@ -1,10 +1,39 @@
 #!/bin/bash
 
-# Reference: http://derekingrouville.com/2012/cscope-will-change-the-way-you-code/
+# Reference: 
+# http://derekingrouville.com/2012/cscope-will-change-the-way-you-code/
+
+hasWget=0
+hasCurl=0
+
+which wget &>/dev/null
+if [ $? -eq 0 ]; then
+  hasWget=1
+fi
+
+which curl &>/dev/null
+if [ $? -eq 0 ]; then
+  hasCurl=1
+fi
+echo "hasWget=$hasWget"
+echo "hasCurl=$hasCurl"
+
+if [ $hasWget == 0 ] && [ $hasCurl == 0 ]; then
+  echo "Both wget/crul commands not found, please install one of them first."
+fi
 
 rm -rf plugin
-#wget http://cscope.sourceforge.net/cscope_maps.vim 
-curl -o cscope_maps.vim http://cscope.sourceforge.net/cscope_maps.vim 
+if [ $hasWget == 1  ]; then
+  wget http://cscope.sourceforge.net/cscope_maps.vim 
+elif [ $hasCurl == 1  ]; then
+  curl -o cscope_maps.vim http://cscope.sourceforge.net/cscope_maps.vim 
+else
+  echo "Unknow parameters, exit"
+  exit 0
+fi
 
 mkdir plugin
 mv cscope_maps.vim plugin
+
+echo "bye!"
+
