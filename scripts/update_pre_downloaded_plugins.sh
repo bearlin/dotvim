cb8096b02cc99380ee2450701a8d4876705613c3
BearLin <bear.lin.001@gmail.com>
2015-03-05 16:38:07 +0800

    Add bundle/autocomplpop/update.sh to scripts/update_pre_downloaded_plugins.sh

--- scripts/update_pre_downloaded_plugins.sh
+++ scripts/update_pre_downloaded_plugins.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
 # cscope_maps
+echo "Updating cscope_maps..."
 cd ~/.vim/bundle/cscope_maps/ 
 ./update.sh
 cd -
 
 # autoload_cscope
+echo "Updating autoload_cscope..."
 cd ~/.vim/bundle/autoload_cscope/  
 ./update.sh
 cd -
 
+# autocomplpop 
+echo "Updating autocomplpop..."
+cd ~/.vim/bundle/autocomplpop/  
+./update.sh
+cd -
+echo "bye!"
