b08dc0e87bac215542ee8d7f8fd64236b7ec8274
BearLin <bear.lin.001@gmail.com>
2015-03-06 13:48:29 +0800

    Add scripts/set_env_git.sh

---
+++ scripts/set_env_git.sh
@@ -0,0 +1,8 @@
+# !/bin/bash
+
+# References:
+# http://git-scm.com/book/en/v2/Getting-Started-First-Time-Git-Setup
+
+git config --global user.name "BearLin"
+git config --global user.email "bear.lin.001@gmail.com"
+
