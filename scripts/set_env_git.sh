065c8f455d10c0adfd8cda30c6aade98ec887d5f
bearlin <bear.lin.001@gmail.com>
2015-03-06 14:36:24 +0800

    Update scripts/set_env_git.sh

--- scripts/set_env_git.sh
+++ scripts/set_env_git.sh
@@ -3,6 +3,16 @@
 # References:
 # http://git-scm.com/book/en/v2/Getting-Started-First-Time-Git-Setup
 
+# Your Identity
 git config --global user.name "BearLin"
 git config --global user.email "bear.lin.001@gmail.com"
+
+# Your Editor
+git config --global core.editor vim
+
+# Checking Your Settings
+#git config --list
+git config user.name
+git config user.email
+git config core.editor
 
