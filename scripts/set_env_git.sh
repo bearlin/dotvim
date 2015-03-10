566d1d4d7036bada9d98db195286fcbf3ea914ce
BearLin <bear.lin.001@gmail.com>
2015-03-10 20:52:21 +0800

    Update scripts/set_env_git.sh

--- scripts/set_env_git.sh
+++ scripts/set_env_git.sh
@@ -5,19 +5,40 @@ source $DOTVIMHOME/scripts/handy_functions.sh
 # References:
 # http://git-scm.com/book/en/v2/Getting-Started-First-Time-Git-Setup
 
+usage() {
+  echo "Usage: " && \
+  echo "./$0 [user.name] [user.email] [core.editor]" 
+}
+
+if [ "$#" -lt 1 ]; then
+  usage
+  byebye
+fi
+
 echo "Update git configurations..."
 
 # Your Identity
-git config --global user.name "BearLin"
-git config --global user.email "bear.lin.001@gmail.com"
+if [ "$1" != "" ]; then
+  git config --global user.name "$1"
+fi
+if [ "$2" != "" ]; then
+  git config --global user.email "$2"
+fi
 
 # Your Editor
-git config --global core.editor vim
+if [ "$3" != "" ]; then
+  git config --global core.editor $3 
+fi
 
 # Checking Your Settings
-#git config --list
+echo ""
+echo "Your current git config setting had been changed to:"
+echo "[name]"
 git config user.name
+echo "[email]"
 git config user.email
+echo "[editor]"
 git config core.editor
+#git config --list
 
 byebye
