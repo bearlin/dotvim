f5d6991dd6685e1e564eec58284ab718ccb99a4b
BearLin <bear.lin.001@gmail.com>
2015-05-31 15:53:12 +0800

    Move all scripts into ./scripts/ folder

--- scripts/update_cscope_maps.sh
+++ scripts/update_cscope_maps.sh
@@ -6,25 +6,27 @@ source $DOTVIMHOME/scripts/handy_functions.sh
 # http://derekingrouville.com/2012/cscope-will-change-the-way-you-code/
 # http://cscope.sourceforge.net/cscope_vim_tutorial.html
 
+cd "$DOTVIMHOME" 
+# ------------------------ 
+
 hasWget=0
 hasCurl=0
 setFlagsHasWgetHasCurlAndExitIfBothEqualZero "$@"
 echo "hasWget=$hasWget"
 echo "hasCurl=$hasCurl"
 
-rm -rf plugin
+rm -rf bundle/cscope_maps/plugin
+mkdir bundle/cscope_maps/plugin
 
 # Current version: http://cscope.sourceforge.net/cscope_maps.vim 
 if [ $hasWget == 1  ]; then
-  wget "http://cscope.sourceforge.net/cscope_maps.vim" -O "cscope_maps.vim"
+  wget "http://cscope.sourceforge.net/cscope_maps.vim" -O "bundle/cscope_maps/plugin/cscope_maps.vim"
 elif [ $hasCurl == 1  ]; then
-  curl -LSso "cscope_maps.vim" "http://cscope.sourceforge.net/cscope_maps.vim"
+  curl -LSso "bundle/cscope_maps/plugin/cscope_maps.vim" "http://cscope.sourceforge.net/cscope_maps.vim"
 else
   die "Unknow parameters, exit"
 fi
 
-mkdir plugin
-mv cscope_maps.vim plugin
-
+# ------------------------ 
+cd -
 byebye "$@"
-
