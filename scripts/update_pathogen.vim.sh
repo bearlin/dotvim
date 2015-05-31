e7b485f4cd1beffa76deb483b8c304ceb0a2dde4
BearLin <bear.lin.001@gmail.com>
2015-05-31 10:50:22 +0800

    Add script to auto update pathogen.vim

---
+++ scripts/update_pathogen.vim.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+DOTVIMHOME=~/.vim
+source $DOTVIMHOME/scripts/handy_functions.sh
+
+# pathogen.vim 
+echo "Updating pathogen.vim in ~/.vim/autoload/..."
+cd $DOTVIMHOME/autoload/
+./update_pathogen.sh
+cd -
+
+byebye
--- scripts/update_pre_downloaded_plugins.sh
+++ scripts/update_pre_downloaded_plugins.sh
@@ -5,19 +5,19 @@ source $DOTVIMHOME/scripts/handy_functions.sh
 # cscope_maps
 echo "Updating cscope_maps..."
 cd $DOTVIMHOME/bundle/cscope_maps/ 
-./update.sh
+./update_cscope_maps.sh
 cd -
 
 # autoload_cscope
 echo "Updating autoload_cscope..."
 cd $DOTVIMHOME/bundle/autoload_cscope/  
-./update.sh
+./update_autoload_cscope.sh
 cd -
 
 # autocomplpop 
 echo "Updating autocomplpop..."
 cd $DOTVIMHOME/bundle/autocomplpop/  
-./update.sh
+./update_autocomplpop.sh
 cd -
 
 byebye
