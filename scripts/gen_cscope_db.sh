8d2b462c5466da2ebf79355463f4f797a2c00523
BearLin <bear.lin.001@gmail.com>
2015-10-22 10:35:58 +0800

    Update usage of scripts/gen_cscope_db.sh scripts/handy_functions.sh scripts/set_env_git.sh

--- scripts/gen_cscope_db.sh
+++ scripts/gen_cscope_db.sh
@@ -6,21 +6,21 @@
 DOTVIMHOME=~/.vim
 source $DOTVIMHOME/scripts/handy_functions.sh
 
-#echo "The script name is        ==> $0"
-#echo "Total parameter number is ==> $#"
-
-[ "$#" -lt 1 ] && \
+usage() {
   echo "Usage: " && \
   echo "Init cscope files: ./gen_cscope_db.sh init" && \
   echo "Init cscope files with tags in /usr/include: ./gen_cscope_db.sh init std" && \
-  echo "Clear cscope files: ./gen_cscope_db.sh clean" && \
+  echo "Init cscope files with ALL (cscope -bqR): ./gen_cscope_db.sh init all" && \
+  echo "Clear cscope files: ./gen_cscope_db.sh clean"
+}
+
+if [ "$#" -lt 1 ]; then
+  echo "011"
+  usage
   byebye "$@"
+fi
 
-#echo "Your whole parameter is   ==> '$@'"
-#echo "The 1st parameter         ==> $1"
-#echo "The 2nd parameter         ==> $2"
-
-if [ "$1" == "init" ] || [ "$1" == "clean" ]; then
+if [ "$1" == "init" ] || [ "$1" == "clean" ] || [ "$1" == "all" ]; then
   if [ "$2" == "std" ]; then
     echo "para=$1 $2"
   else
@@ -30,14 +30,28 @@ else
   die "Unknow parameters \"$1\", exit!"
 fi
 
-if [ "$1" == "init" ]; then
+hasCscope=0
+setFlagsHasCscopeAndExitIfEqualZero "$@"
+echo "hasCscope=$hasCscope"
+
+if [ "$1" == "clean" ]; then
+  echo "clean generated cscope files in current folder..."
+  rm cscope.*
+elif [ "$1" == "init" ] && [ "$2" == "all" ]; then
+  echo "Build the database with ALL (cscope -bqR)..."
+  # -R     Recurse subdirectories during search for source files.
+  # -b     Build the cross-reference only.
+  # -q     Enable fast symbol lookup via an inverted index.
+  cscope -bqR
+elif [ "$1" == "init" ]; then
   # References:
   # Cscope Will Change The Way You Code With Vim:
   # http://derekingrouville.com/2012/cscope-will-change-the-way-you-code/
   # vim附件：cscope+ctag 使用筆記
   # http://angledark0123.pixnet.net/blog/post/51919594-vim%E9%99%84%E4%BB%B6%EF%BC%9Acscope%2Bctag-%E4%BD%BF%E7%94%A8%E7%AD%86%E8%A8%98
-  
+
   # 1. Build cscope.files
+  # --------------------------------------------------------------------------------------------------------------
   echo "Build cscope.files..."
     # C/C++ files
     #  -name "*.c" -o -name "*.cc" -o -name "*.h" -o -name "*.hpp" -o -name "*.cpp" -o -name "*.cxx" -o -name "*.inl"\
@@ -62,21 +76,22 @@ if [ "$1" == "init" ]; then
   #find `pwd`/AppLoaders/ `pwd`/Kits/ `pwd`/Platform/ `pwd`/Products/ \
   find `pwd` \
       -name "*.c" -o -name "*.cc" -o -name "*.h" -o -name "*.hpp" -o -name "*.cpp" -o -name "*.cxx" -o -name "*.inl"\
+      -o -name "*.py" \
       -o -name "*.pl" \
       -o -name "*.sh" -o -name "*.bat" -o -name "*.cmd" \
       -o -name "*.yaml" -o -name "*.xml" -o -name "*.json" -o -name "*.ini" \
       -o -name "*.cmake" -o -name "CMakeLists.txt" -o -name "*.in"\
+      -o -name "*.ui" -o -name "*.qss" -o -name "*.qrc" -o -name "*.ts" \
       -o -name "*.ric" \
   | sed -e 's/^/"/g' -e 's/$/"/g' \
   > cscope.files
-# [NOTE]
-# Issue: man cscope :Filenames in the namefile that contain whitespace have to be enclosed in "double quotes".  Inside such quoted filenames, any  double-quote and backslash characters have to be escaped by backslashes
-# Solution: quote all file paths: http://stackoverflow.com/questions/3196760/how-to-use-cscope-with-paths-that-contain-spaces
+  # [NOTE]
+  # Issue: man cscope :Filenames in the namefile that contain whitespace have to be enclosed in "double quotes".  Inside such quoted filenames, any  double-quote and backslash characters have to be escaped by backslashes
+  # Solution: quote all file paths: http://stackoverflow.com/questions/3196760/how-to-use-cscope-with-paths-that-contain-spaces
+  # --------------------------------------------------------------------------------------------------------------
 
   # 2. Build the database
-  which cscope &>/dev/null
-  [ $? -eq 0 ] || echo "cscope command not found, please install cscope first."
-
+  # --------------------------------------------------------------------------------------------------------------
   echo "Build the database..."
   # -R     Recurse subdirectories during search for source files.
   # -b     Build the cross-reference only.
@@ -94,9 +109,10 @@ if [ "$1" == "init" ]; then
     cscope -bqk -i cscope.files
     echo "WITHOUT tags in std headers"
   fi
-elif [ "$1" == "clean" ]; then
-  echo "clean generated cscope files in current folder..."
-  rm cscope.*
+  # --------------------------------------------------------------------------------------------------------------
+else
+  usage
+  byebye "$@"
 fi
 
 byebye "$@"
