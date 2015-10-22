b744ae0072e6c9d61e7050cde8653658c86753b2
bearlin <bear.lin.001@gmail.com>
2015-10-22 10:35:58 +0800

    Update usage of scripts/gen_cscope_db.sh scripts/handy_functions.sh scripts/set_env_git.sh

--- scripts/handy_functions.sh
+++ scripts/handy_functions.sh
@@ -55,6 +55,24 @@ hasCommandUnzip() {
   fi
   return "$retval"
 }
+hasCommandCscope() {
+  # Ref: http://stackoverflow.com/questions/8742783/returning-value-from-called-function-in-shell-script
+  retval=0
+  which cscope &>/dev/null
+  if [ $? -eq 0 ]; then
+    retval=1
+  fi
+  return "$retval"
+}
+hasCommandCtags() {
+  # Ref: http://stackoverflow.com/questions/8742783/returning-value-from-called-function-in-shell-script
+  retval=0
+  which ctags &>/dev/null
+  if [ $? -eq 0 ]; then
+    retval=1
+  fi
+  return "$retval"
+}
 setFlagsHasWgetHasCurlAndExitIfBothEqualZero() {
   # Ref: http://stackoverflow.com/questions/8742783/returning-value-from-called-function-in-shell-script
   hasCommandWget
@@ -71,5 +89,21 @@ setFlagsHasUnzipAndExitIfEqualZero() {
   hasUnzip=$?
   if [ $hasUnzip == 0 ]; then
     byebye "$@" "unzip commands not found, please install unzip first."
+  fi
+}
+setFlagsHasCscopeAndExitIfEqualZero() {
+  # Ref: http://stackoverflow.com/questions/8742783/returning-value-from-called-function-in-shell-script
+  hasCommandCscope
+  hasCscope=$?
+  if [ $hasCscope == 0 ]; then
+    byebye "$@" "cscope commands not found, please install cscope first."
+  fi
+}
+setFlagsHasCtagsAndExitIfEqualZero() {
+  # Ref: http://stackoverflow.com/questions/8742783/returning-value-from-called-function-in-shell-script
+  hasCommandCtags
+  hasCtags=$?
+  if [ $hasCtags== 0 ]; then
+    byebye "$@" "ctags commands not found, please install ctags first."
   fi
 }
