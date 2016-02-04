585c83ee251f10ed6b7f445f9c1f011149f12507
bearlin <bear.lin.001@gmail.com>
2016-02-04 16:01:21 +0800

    Add htags support to scripts/gen_gtags_db.sh

--- scripts/gen_gtags_db.sh
+++ scripts/gen_gtags_db.sh
@@ -7,7 +7,8 @@ usage() {
   echo "Usage: " && \
   echo "Init gtags files: ./gen_gtags_db.sh init" && \
   echo "Update gtags files: ./gen_gtags_db.sh update" && \
-  echo "Clear gtags files: ./gen_gtags_db.sh clean"
+  echo "Clear gtags files: ./gen_gtags_db.sh clean" \
+  echo "Generate html files: ./gen_gtags_db.sh html"
 }
 
 if [ "$#" -lt 1 ]; then
@@ -15,7 +16,10 @@ if [ "$#" -lt 1 ]; then
   byebye "$@"
 fi
 
-if [ "$1" == "init" ] || [ "$1" == "update" ] || [ "$1" == "clean" ]; then
+if [ "$1" == "init" ] || 
+   [ "$1" == "update" ] || 
+   [ "$1" == "clean" ] || 
+   [ "$1" == "html" ]; then
   echo "para=$1"
 else
   die "Unknow parameters \"$1\", exit!"
@@ -39,6 +43,19 @@ elif [ "$1" == "init" ]; then
 elif [ "$1" == "update" ]; then
   echo "Update the database with (global -u)..."
   global -u
+elif [ "$1" == "html" ]; then
+  echo "Generate a hypertext from a set of source files with (htags --suggest)..."
+  echo "Htags generates a hypertext from a set of source files of C, C++, Yacc,
+  Java, PHP and Assembly. In  advance  of  using  this  command, you should
+  execute gtags(1) in the root directory of a source project.  Then you can
+  execute htags in the same place.  Htags makes a directory named ´HTML´, and
+  puts a hypertext in it.  You can start browsing at ´HTML/index.html´. Since
+  htags generates a static hypertext as long as the -D or -f option is not
+  specified, you can move it anywhere and browse it by any browser without any
+  HTTP server. This command has so many options.  If you are new to htags, it
+  is recommended to use the --suggest option.  With that option, htags chooses
+  popular options on your behalf."
+  htags --suggest
 else
   usage
   byebye "$@"
