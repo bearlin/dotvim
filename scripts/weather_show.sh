e6fe0d0cf396cbe89bbaa8c1c069a0112291d8d2
bearlin <bear.lin.001@gmail.com>
2016-04-29 17:01:49 +0800

    Update and rename scripts/weather_show_taipei.sh -> scripts/weather_show.sh

--- scripts/weather_show.sh
+++ scripts/weather_show.sh
@@ -5,12 +5,25 @@ source $DOTVIMHOME/scripts/handy_functions.sh
 # Reference: 
 # https://github.com/chubin/wttr.in
 
+usage() {
+  echo "Usage: " && \
+  echo "Show weather of Taipei city: ./$0"
+  echo "Show weather of [city]: ./$0 [city]"
+}
+
 hasCommandCurl
 hasCurl=$?
 # echo "hasCurl=$hasCurl"
 
 if [ $hasCurl == 1  ]; then
-  curl http://wttr.in/taipei
+  CITY="Taipei"
+  # echo "#=$#"
+  if [ "$#" -eq 1 ]; then
+    CITY="$1"
+  fi
+  # echo "CITY=$CITY"
+
+  curl http://wttr.in/"$CITY"
 else
   die "Unknow parameters, exit"
 fi
